34d5f15ecc7ec9cac0fd45dce0603cdfdc8205e2
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-10-29 22:53:01 -0400

    changed port and added npm start

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const temperature=require('./address')
 const app=express();
-const port=3000;
+const port=process.env.PORT||3000;
 
 //to handle json data in post request
 app.use(express.json())
